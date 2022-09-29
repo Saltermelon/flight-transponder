aace79885021eba0757d6b222669cfde17a2c4ec
Saltermelon <saltermelon@protonmail.com>
2022-09-28 23:55:20 -0400

    Moved swep_transponder to a folder

---
+++ swep_transponder/swep_transponder.lua
@@ -0,0 +1,33 @@
+AddCSLuaFile()
+
+SWEP.Author             =   "Saltermelon"
+SWEP.Base               =   "weapon_base"
+SWEP.PrintName          =   "Transponder"
+--TODO
+SWEP.Instructions       =   [[
+
+]]
+--TODO
+SWEP.ViewModel          =   ""
+SWEP.ViewModelFlip      =   false
+-- Subject to change if it would look better with the hands
+SWEP.UseHands           =   false
+--TODO
+SWEP.WorldModel         =   ""
+--TODO, maybe pistol?
+SWEP.SetHoldType        =   ""
+SWEP.Weight             =   "1"
+--If having problems, try switching
+SWEP.AutoSwitchTo       =   true
+SWEP.AutoSwitchFrom     =   false
+--Look for other type of grip if pistol looks odd
+SWEP.SetHoldType        =   "pistol"
+SWEP.Weight             =   "1"
+--Change these around if something is broken
+SWEP.AutoSwitchTo       =   true
+SWEP.AutoSwitchFrom     =   false
+SWEP.Slot               =   4
+SWEP.SlotPos            =   996
+SWEP.DrawAmmo           =   false
+SWEP.DrawCrosshair      =   false
+SWEP.Spawnable          =   true
